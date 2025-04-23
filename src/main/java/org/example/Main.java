e6e8331a91e359b250b6f5a71624aa32335cdb57
vik-4994 <igor.gavrylenko05@gmail.com>
2025-04-24 00:45:44 +0200

    FInal solar system

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,47 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import com.sun.j3d.utils.geometry.Sphere;
 import com.sun.j3d.utils.image.TextureLoader;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
-import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.geometry.Primitive;
+import com.sun.j3d.utils.universe.ViewingPlatform;
 
 import javax.media.j3d.*;
 import javax.vecmath.*;
 
 import java.awt.*;
 import javax.swing.*;
-import java.util.Enumeration;
 
 public class Main extends JFrame {
 
     public Main() {
-        // Налаштування вікна
         setTitle("Solar System - Java 3D");
         setSize(800, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // Canvas 3D
         Canvas3D canvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
         getContentPane().add("Center", canvas3D);
 
-        // Простір (універсум)
         SimpleUniverse universe = new SimpleUniverse(canvas3D);
-        universe.getViewingPlatform().setNominalViewingTransform();
+        ViewingPlatform vp = universe.getViewingPlatform();
+        TransformGroup viewTransform = vp.getViewPlatformTransform();
 
-        // Орбітальна поведінка (управління камерою мишкою)
+        Transform3D view = new Transform3D();
+        Vector3f eye = new Vector3f(1.8f, 1.2f, 2.8f);
+        view.lookAt(
+                new Point3d(eye),
+                new Point3d(0, 0, 0),
+                new Vector3d(0, 1, 0)
+        );
+        view.invert();
+        viewTransform.setTransform(view);
+
+
         OrbitBehavior orbit = new OrbitBehavior(canvas3D);
         orbit.setSchedulingBounds(new BoundingSphere());
         universe.getViewingPlatform().setViewPlatformBehavior(orbit);
 
-        // Головна сцена
         BranchGroup scene = createSceneGraph();
         scene.compile();
         universe.addBranchGraph(scene);
@@ -46,7 +50,6 @@ public class Main extends JFrame {
     private BranchGroup createSceneGraph() {
         BranchGroup root = new BranchGroup();
 
-        // Світло
         BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
         Color3f lightColor = new Color3f(1.0f, 1.0f, 1.0f);
 
@@ -58,20 +61,17 @@ public class Main extends JFrame {
         pointLight.setInfluencingBounds(bounds);
         root.addChild(pointLight);
 
-        // Сонце (жовта сфера)
         Appearance sunApp = new Appearance();
         ColoringAttributes sunColor = new ColoringAttributes(new Color3f(Color.YELLOW), ColoringAttributes.SHADE_FLAT);
         sunApp.setColoringAttributes(sunColor);
-        Sphere sun = new Sphere(0.3f, Primitive.GENERATE_NORMALS, 50, sunApp);
+        Sphere sun = createTexturedPlanet("textures/sun.jpeg", 0.3f);
         root.addChild(sun);
 
-        // Планета (наприклад, Земля)
-        // Планета (наприклад, Земля)
         Appearance earthApp = new Appearance();
         earthApp.setColoringAttributes(new ColoringAttributes(new Color3f(Color.BLUE), ColoringAttributes.SHADE_FLAT));
-        Sphere earth = new Sphere(0.1f, Primitive.GENERATE_NORMALS, 50, earthApp);
+        Sphere earth = createTexturedPlanet("textures/earth.jpg", 0.1f);
 
-// Орбіта Землі
+
         TransformGroup orbitEarth = new TransformGroup();
         orbitEarth.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
@@ -82,7 +82,6 @@ public class Main extends JFrame {
 
         orbitEarth.addChild(earthMove);
 
-        // Місяць
         Appearance moonApp = new Appearance();
         Material moonMat = new Material(
                 new Color3f(Color.LIGHT_GRAY),
@@ -93,9 +92,9 @@ public class Main extends JFrame {
         );
         moonApp.setMaterial(moonMat);
 
-        Sphere moon = new Sphere(0.03f, Primitive.GENERATE_NORMALS, 50, moonApp);
+        Sphere moon = createTexturedPlanet("textures/moon.jpg", 0.03f);
 
-// Орбіта Місяця навколо Землі
+
         TransformGroup moonOrbit = new TransformGroup();
         moonOrbit.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         Transform3D moonPos = new Transform3D();
@@ -104,18 +103,49 @@ public class Main extends JFrame {
         moonMove.addChild(moon);
         moonOrbit.addChild(moonMove);
 
-// Додати анімацію обертання Місяця
         Alpha moonAlpha = new Alpha(-1, 2000);
         RotationInterpolator moonRot = new RotationInterpolator(moonAlpha, moonOrbit);
         moonRot.setSchedulingBounds(new BoundingSphere());
 
-// Додати місячну орбіту до Землі
         earthMove.addChild(moonOrbit);
         earthMove.addChild(moonRot);
 
+        Sphere venus = createTexturedPlanet("textures/venus.jpg", 0.09f);
+        TransformGroup orbitVenus = new TransformGroup();
+        orbitVenus.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+
+        Transform3D venusPos = new Transform3D();
+        venusPos.setTranslation(new Vector3f(0.5f, 0f, 0f));
+        TransformGroup venusMove = new TransformGroup(venusPos);
+        venusMove.addChild(venus);
+        orbitVenus.addChild(venusMove);
+
+        Alpha venusAlpha = new Alpha(-1, 7000);
+        RotationInterpolator rotVenus = new RotationInterpolator(venusAlpha, orbitVenus);
+        rotVenus.setSchedulingBounds(new BoundingSphere());
+
+        root.addChild(orbitVenus);
+        root.addChild(rotVenus);
+
+        Sphere saturn = createTexturedPlanet("textures/saturn.jpeg", 0.15f);
+
+        TransformGroup orbitSaturn = new TransformGroup();
+        orbitSaturn.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+
+        Transform3D saturnPos = new Transform3D();
+        saturnPos.setTranslation(new Vector3f(1.5f, 0f, 0f));
+        TransformGroup saturnMove = new TransformGroup(saturnPos);
+        saturnMove.addChild(saturn);
+        orbitSaturn.addChild(saturnMove);
+
+        Alpha saturnAlpha = new Alpha(-1, 12000);
+        RotationInterpolator rotSaturn = new RotationInterpolator(saturnAlpha, orbitSaturn);
+        rotSaturn.setSchedulingBounds(new BoundingSphere());
+
+        root.addChild(orbitSaturn);
+        root.addChild(rotSaturn);
 
 
-        // Анімація обертання Землі навколо Сонця
         Alpha earthAlpha = new Alpha(-1, 5000);
         RotationInterpolator rotEarth = new RotationInterpolator(earthAlpha, orbitEarth);
         rotEarth.setSchedulingBounds(new BoundingSphere());
@@ -128,7 +158,11 @@ public class Main extends JFrame {
 
     private Sphere createTexturedPlanet(String texturePath, float radius) {
         Appearance appearance = new Appearance();
-        TextureLoader loader = new TextureLoader(texturePath, "RGB", new Container());
+        TextureLoader loader = new TextureLoader(
+                getClass().getClassLoader().getResource(texturePath),
+                "RGB", new Container()
+        );
+
         Texture texture = loader.getTexture();
         appearance.setTexture(texture);
 
